240163c3bbc4f284752ba9b947b6b30f264973d4
PriyankaDathriB <74810406+PriyankaDathriB@users.noreply.github.com>
2021-12-31 12:02:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyANod-iiMAnhLEz2pqiR9uLbaZ5CHNUu64",
+    authDomain: "storytelling-5cbc2.firebaseapp.com",
+    projectId: "storytelling-5cbc2",
+    storageBucket: "storytelling-5cbc2.appspot.com",
+    messagingSenderId: "415368797904",
+    appId: "1:415368797904:web:f9a0bbf53ae9aec1cc88d5"
+  };
+  
